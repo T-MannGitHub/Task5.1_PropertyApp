88a0d338e9881cc5712ea10fffcbc569ea809782
TKMann <tiffany.mann@radicalsystems.com.au>
2023-05-14 14:47:52 +0930

    Improved image view Created Fragments for each property

--- app/src/main/java/com/tmannapps/propertyapp/MainActivity.java
+++ app/src/main/java/com/tmannapps/propertyapp/MainActivity.java
@@ -1,11 +1,15 @@
 package com.tmannapps.propertyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import java.security.cert.CertPathBuilderSpi;
@@ -38,7 +42,6 @@ public class MainActivity extends AppCompatActivity implements PropertyAdapter.O
         for (int i = 0; i < imageList.length; i++) {
             com.tmannapps.propertyapp.Property property = new com.tmannapps.propertyapp.Property(i, imageList[i], addressList[i], rentList[i], poolList[i], petsList[i], bedroomsList[i]);
             propertyList.add(property);
-
         }
     }
 
@@ -46,18 +49,26 @@ public class MainActivity extends AppCompatActivity implements PropertyAdapter.O
     @Override
     public void onItemClick(int position) {
         //todo: set the onclick listener to go to a fragment per case instead of a Toast
+        Fragment fragment;
+
         switch (position) {
             case 0:
-
+                fragment = new MarinaFragment();
                 Toast.makeText(this, "You clicked on Marina Blv", Toast.LENGTH_SHORT).show();
                 break;
             case 1:
-
+                fragment = new CharlotteFragment();
+                fragment.isVisible();
                 Toast.makeText(this, "You clicked on Charlotte St", Toast.LENGTH_SHORT).show();
                 break;
             default:
                 throw new IllegalStateException("Unexpected value: " + position);
         }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.myFragment, fragment).addToBackStack(null).commit();
+
 
     }
 }
